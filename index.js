d9eec98368ba5f02c6d6a10d89a959f433fb66e8
mikzog <magicmikevv@gmail.com>
2020-02-29 09:56:16 +0100

    update webhook

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const token = '0123456789'; // type here your verification token
 const username = "1b923fa4c4f4b38c56b9e327e5f1945f";
 const password = "680b6c251942759b4bd667e7b1a9cedf";
 const shop = "time-rediscovered";
-const api_version = "2019-10";
+const api_version = "2020-01";
 const resource = "graphql.json";
 
 app.listen(3000, () => console.log('[ChatBot] Webhook is listening'));
@@ -34,30 +34,21 @@ app.post('/', (req, res) => {
   const params = req.body["result"]["sessionParameters"];
   console.log(params);
 
-  var gender = params.Gender;
-  var make = params.Make;
-  var budget = params.Spend;
-
-  if (budget < 1500) {
-    budget = "Less than £1500"
-  } else if (budget >= 1500 && budget < 3000) {
-    budget = "£1500 - £3000"
-  } else if (budget >= 3000 && budget < 5000) {
-    budget = "£3000 - £5000"
-  } else if (budget >= 3000 && budget < 5000) {
-    budget = "£5000 - £10000"
-  } else {
-    budget = "More than £10000"
-  }
+  var gender = params.Gender ? "tag:"+params.Gender : "";
+  var make = params.Make ? "tag:"+params.Make : "";
+  var budget = params.Spend ? "tag:"+params.Spend : "";
+  var style = params.Style ? "tag:"+params.Style : "";
 
   const headers = {
     'Content-Type': 'application/graphql',
     'X-Shopify-Access-Token': password
   };
 
+  const condition = params.Soldout == "yes" ? ":0" : ":>0";
+
   const queryString = `
   {
-    products(first:5, query:"published_status:published AND available_for_sale:true ${gender} ${make} ${budget}") {
+    products(first:5, query:"published_status:published inventory_total${condition} ${gender} ${make} ${budget} ${style}") {
       edges {
         node {
           id
@@ -95,17 +86,14 @@ app.post('/', (req, res) => {
         });
         let elements = [];
         for (let product of products) {
-          console.log(product.node.onlineStoreUrl);
-          console.log(product.node.totalInventory);
-          console.log(product.node.inventoryQuantity);
-          console.log(product.node.availableForSale);
+          console.log(product.node.title);
           elements.push({
             "title": product.node.title,
             "imageUrl": product.node.featuredImage.originalSrc,
             "buttons": [
               {
                 "type": "url",
-                "title": "Shop Now",
+                "title": params.Soldout == "yes" ? "Check More" : "Shop Now",
                 "value": product.node.onlineStoreUrl
               }
             ]
